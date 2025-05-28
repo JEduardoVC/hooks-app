c16caac2834a36c67e6626fa1a3692aabcc3addd
JEduardoVC <eduardo.valencia.camacho@gmail.com>
2025-05-27 23:14:42 -0600

    <feat> add examples with useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import './index.css';
 import { HooksApp } from './HooksApp.jsx';
 import { CounterApp } from './01-useState/CounterApp.jsx';
 import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook.jsx';
+import { SimpleForm } from './02-useEffect/SimpleForm.jsx';
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <HooksApp></HooksApp> */}
     {/* <CounterApp></CounterApp>*/}
-    <CounterWithCustomHook></CounterWithCustomHook>
+    {/* <CounterWithCustomHook></CounterWithCustomHook> */}
+    {/* <SimpleForm></SimpleForm> */}
+    <FormWithCustomHook></FormWithCustomHook>
   </StrictMode>,
 )
