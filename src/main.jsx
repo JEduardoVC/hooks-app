0ae19151fdff0b7739a879828c29437b3249576d
JEduardoVC <eduardo.valencia.camacho@gmail.com>
2025-05-28 20:55:05 -0600

    <feat> useCallback

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import { FocusScreen } from './04-useRef/FocusScreen.jsx';
 import { Layout } from './05-useLayoutEffect/Layout.jsx';
 import { Memorize } from './06-memos/Memorize.jsx';
 import { MemorizeHook } from './06-memos/MemorizeHook.jsx';
+import { CallbackHook } from './06-memos/CallbackHook.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -23,6 +24,7 @@ createRoot(document.getElementById('root')).render(
     {/* <FocusScreen></FocusScreen> */}
     {/* <Layout></Layout> */}
     {/* <Memorize></Memorize> */}
-    <MemorizeHook></MemorizeHook>
+    {/* <MemorizeHook></MemorizeHook> */}
+    <CallbackHook></CallbackHook>
   </StrictMode>,
 )
