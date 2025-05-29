349c46b69668a4cd2a4fe64a2dbd835401ee939a
JEduardoVC <eduardo.valencia.camacho@gmail.com>
2025-05-28 21:10:53 -0600

    <feat> Exersice of memo

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import { Layout } from './05-useLayoutEffect/Layout.jsx';
 import { Memorize } from './06-memos/Memorize.jsx';
 import { MemorizeHook } from './06-memos/MemorizeHook.jsx';
 import { CallbackHook } from './06-memos/CallbackHook.jsx';
+import { Padre } from './07-tarea-memo/Padre.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -25,6 +26,7 @@ createRoot(document.getElementById('root')).render(
     {/* <Layout></Layout> */}
     {/* <Memorize></Memorize> */}
     {/* <MemorizeHook></MemorizeHook> */}
-    <CallbackHook></CallbackHook>
+    {/* <CallbackHook></CallbackHook> */}
+    <Padre></Padre>
   </StrictMode>,
 )
